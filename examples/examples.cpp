4a0b75e60d89b68b7b2acf5895dad4f130068393
daniel.schuetz <daniel.schuetz@plugin-alliance.com>
2022-09-08 15:49:35 +0200

    feat: Added example code

---
+++ examples/examples.cpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2022 Daniel Schütz. All rights reserved.
+// MIT License
+
+#include <salzaverde/uri.h>
+#include <iostream>
+
+void printLine(const std::string& text) {
+    std::cout << text << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    //Parsing
+    auto myRawUri = std::string("https://myHomepage.com:1176/my/music?added=last");
+    auto uri = salzaverde::URI::parse(myRawUri);
+    
+    //Access the uri's elements
+    auto scheme = uri->getScheme();
+    auto host = uri->getHost();
+    auto port = uri->getPort();
+    auto path = uri->getPath();
+    auto query = uri->getQuery();
+    auto queryString = query->dump();
+    
+    //Add or replace a query parameter
+    query->set("sortby", "name");
+    
+    //Retrieve a query parameter value by key
+    auto sortbyValue = query->get("sortby");
+    
+    //A value for this key might not exist
+    if(! sortbyValue) {
+        //Do some error handling
+    }
+       
+    //We can also build a new query string in place
+    auto newQuery = salzaverde::URI::Query::build({
+        {"list", "songs"},
+        {"include", "rhcp"}
+    });
+
+    //You can check if a key exists
+    auto containsListKey = newQuery->contains("list");
+    auto keys = query->listKeys(); //Returns a set of keys
+    
+    //And replace it in an existing uri
+    uri->setQuery(std::move(newQuery));
+    
+    //Print results
+    printLine("Parsing " + myRawUri + ":");
+    printLine("");
+    printLine("Scheme is: " + scheme);
+    printLine("Host is: " + host);
+    printLine("Port is: " + port);
+    printLine("Path is: " + path);
+    printLine("Query string is: " + queryString);
+    printLine("");
+    
+    printLine("Parsing Query: " + uri->getQuery()->dump());
+    printLine("Query contains \"list\" key: " + (containsListKey? std::string("true") : std::string("false")));
+    printLine("");
+    printLine("Uri with replaced QueryString: " + uri->dump());
+    printLine("");
+}

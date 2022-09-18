aa6f1dab9e667cf6a7fc1a46637837ded4ad77f1
daniel <daniel.schuetz.lrt@gmail.com>
2022-09-18 10:25:18 +0200

    refactor: Query parser is also jsut a simple struct

--- include/salzaverde/query.h
+++ include/salzaverde/query.h
@@ -12,33 +12,36 @@
 namespace salzaverde {
     /**
      * @brief A query string parser which expects a query string in the form of
-     * "?key1=value1&key2=value2" or "key1=value1&key2=value2"
+     * "key1=value1&key2=value2"
      *
      * Key-value pairs are called parameters and a "Query" object can also be generated
      * from a map representing a number of parameters.
      */
-    class Query {
+    struct Query {
     public:
-        virtual ~Query() {}
+        /**
+         * @brief The query parameters
+         */
+        std::map<std::string, std::string> parameters {};
+
+        /**
+         * @brief Construct an empty query object
+         */
+        Query() = default;
+
+        /**
+         * @brief Construct a query object from a map of parameters
+         */
+        explicit Query(const std::map<std::string, std::string> &parameters);
         
         /**
-         * @brief Factories
+         * @brief Parses a raw query string
          */
-        static std::unique_ptr<Query> parse(const std::string &raw);
-        static std::unique_ptr<Query> build(const std::map<std::string, std::string> &parameters);
+        static Query parse(const std::string &raw);
         
         /**
-         * @brief Returns a string representation of the query object including the preceding question mark.
+         * @brief Returns a query string
          */
-        virtual std::string dump() = 0;
-        
-        /**
-         * @brief Operations
-         */
-        virtual std::optional<std::string> get(const std::string &key) = 0;
-        virtual void set(const std::string &key, const std::string &value) = 0;
-        virtual void erase(const std::string &key) = 0;
-        virtual bool contains(const std::string &key) = 0;
-        virtual std::set<std::string> listKeys() = 0;
+        virtual std::string dump();
     };
 }
--- src/query.cpp
+++ src/query.cpp
@@ -5,87 +5,41 @@
 #include <vector>
 
 namespace salzaverde {
-    static const std::string query_prefix = "?";
+    Query::Query(const std::map<std::string, std::string> &parameters) : parameters(parameters) {};
 
-    class QueryImpl : public Query {
-    public:
-        explicit QueryImpl(const std::string &raw) {
-            parse(raw);
-        }
-        
-        explicit QueryImpl(const std::map<std::string, std::string> &params) : _params(params) {}
-        
-        virtual std::string dump() override {
-            if(_params.empty())
-                return "";
-            
-            std::string queryString = query_prefix;
-            auto it = _params.begin();
-            queryString += it->first + "=" + it->second;
-            while(++it != _params.end()) {
-                queryString += "&" + it->first + "=" + it->second;
-            }
-            return queryString;
-        }
-        
-        virtual std::optional<std::string> get(const std::string &key) override {
-            if(_params.contains(key))
-                return _params[key];
-            
-            return {};
-        }
-        
-        virtual void set(const std::string &key, const std::string &value) override {
-            _params[key] = value;
-        }
-        
-        virtual void erase(const std::string &key) override {
-            _params.erase(key);
-        }
-        
-        virtual bool contains(const std::string &key) override {
-            return _params.contains(key);
-        }
-        
-        virtual std::set<std::string> listKeys() override {
-            std::set<std::string> keys;
-            for(auto &param : _params)
-                keys.emplace(param.first);
-            return keys;
-        }
-        
-    private:
-        std::map<std::string, std::string> _params;
-        
-        void parse(const std::string& queryString) {
-            if(queryString.empty())
-                return;
+    Query Query::parse(const std::string& queryString) {
+        if(queryString.empty())
+            return Query();
 
-            std::string remaining = queryString;
-
-            if(remaining.starts_with("?"))
-                remaining.erase(remaining.begin());
-            
-            std::vector<std::string> keyValuePairs;
-            size_t pos = 0;
-            while(pos != std::string::npos) {
-                pos = remaining.find("&");
-                keyValuePairs.push_back(remaining.substr(0, pos));
-                remaining = remaining.substr(pos + 1);
-            }
-            
-            for(auto &pair : keyValuePairs) {
-                pos = pair.find("=");
-                _params.emplace(pair.substr(0, pos), pair.substr(pos + 1));
-            }
+        Query query;
+        std::string remaining = queryString;
+        
+        std::vector<std::string> keyValuePairs;
+        size_t pos = 0;
+        while(pos != std::string::npos) {
+            pos = remaining.find("&");
+            keyValuePairs.push_back(remaining.substr(0, pos));
+            remaining = remaining.substr(pos + 1);
         }
-    };
+        
+        for(auto &pair : keyValuePairs) {
+            pos = pair.find("=");
+            query.parameters.emplace(pair.substr(0, pos), pair.substr(pos + 1));
+        }
 
-    std::unique_ptr<Query> Query::parse(const std::string &raw) {
-        return std::make_unique<QueryImpl>(raw);
+        return query;
     }
 
-    std::unique_ptr<Query> Query::build(const std::map<std::string, std::string> &params) {
-        return std::make_unique<QueryImpl>(params);
+    std::string Query::dump() {
+        if(parameters.empty())
+            return "";
+        
+        std::string queryString = "";
+        auto it = parameters.begin();
+        queryString += it->first + "=" + it->second;
+        while(++it != parameters.end()) {
+            queryString += "&" + it->first + "=" + it->second;
+        }
+        return queryString;
     }
 }

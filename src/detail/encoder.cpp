c92d9c394c5a90377d4797c9fc14d48cf90133f6
daniel.schuetz <daniel.schuetz@plugin-alliance.com>
2022-11-07 15:01:17 +0100

    feat: Added URL decoding with tests

--- src/detail/encoder.cpp
+++ src/detail/encoder.cpp
@@ -3,19 +3,72 @@
 
 #include <salzaverde/detail/encoder.h>
 
+#include <iostream>
+#include <regex>
+
 namespace salzaverde {
+	class Regex {
+	public:
+		struct Match {
+			std::string content {};
+			std::vector<std::string> groups {};
+		};
+		
+		Regex(const std::regex& regex) : _regex(regex) {}
+		
+		std::vector<Match> search(const std::string& input) {
+			std::vector<Match> matches;
+			std::smatch smatch;
+			std::string remaining = input;
+			while(regex_search(remaining, smatch, _regex)) {
+				auto match = smatchToMatch(smatch);
+				matches.push_back(match);
+				remaining = smatch.suffix();
+			}
+			return matches;
+		}
+		
+	private:
+		std::regex _regex;
+		
+		Match smatchToMatch(const std::smatch &smatch) {
+			Match output;
+			output.content = smatch.str();
+			for(auto group : smatch) {
+				output.groups.push_back(group.str());
+			}
+			return output;
+		}
+	};
+	
+	static std::string hexToString(const std::string &hex) {
+		int len = hex.length();
+		std::string output;
+		for(int i=0; i< len; i+=2) {
+			std::string byte = hex.substr(i,2);
+			char chr = (char) (int) strtol(byte.c_str(), nullptr, 16);
+			output.push_back(chr);
+		}
+		return output;
+	}
+	
 	std::string URLEncoding::encode(const std::string &raw) {
 		return "";
 	}
 	
 	std::string URLEncoding::decode(const std::string &encoded) {
-		int len = encoded.length();
-		std::string str;
-		for(int i=0; i< len; i+=2) {
-			std::string byte = encoded.substr(i,2);
-			char chr = (char) (int) strtol(byte.c_str(), nullptr, 16);
-			str.push_back(chr);
+		auto regex = Regex(std::regex(R"((.*?)(%[a-fA-F0-9]{2})|(.+))"));
+		auto matches = regex.search(encoded);
+		
+		std::string result = "";
+		
+		for(auto &match : matches) {
+			assert(match.groups.size() == 4);
+			if(! match.groups[1].empty()) result += match.groups[1];
+			if(! match.groups[2].empty()) result += hexToString(match.groups[2].substr(1));
+			if(! match.groups[3].empty()) result += match.groups[3];
 		}
-		return str;
+		
+		return result;
 	}
 }
--- test/encoder_test.cpp
+++ test/encoder_test.cpp
@@ -6,50 +6,58 @@
 
 using namespace salzaverde;
 
-/* What do we want?
- - percent encoded URL components should be decoded during parsing
- - URL components containing illegal characters should be percent-encoded
- */
-
 TEST(URLDecoding, Space) {
 	auto hexString = std::string("%20");
 	auto result = URLEncoding::decode(hexString);
-	
+
 	EXPECT_EQ(result, " ");
 }
 
 TEST(URLEncoding, Space) {
-	auto hexString = std::string(" ");
-	auto result = URLEncoding::encode(hexString);
-	
+	auto rawString = std::string(" ");
+	auto result = URLEncoding::encode(rawString);
+
 	EXPECT_EQ(result, "%20");
+}
+
+TEST(URLDecoding, TwoSpaces) {
+	auto hexString = std::string("%20%20");
+	auto result = URLEncoding::decode(hexString);
+
+	EXPECT_EQ(result, "  ");
+}
+
+TEST(URLEncoding, TwoSpaces) {
+	auto hexString = std::string("  ");
+	auto result = URLEncoding::encode(hexString);
+
+	EXPECT_EQ(result, "%20%20");
 }
 
 TEST(URLDecoding, CopyrightCharacter) {
 	auto hexString = std::string("%c2%a9");
 	auto result = URLEncoding::decode(hexString);
-	
+
 	EXPECT_EQ(result, "©");
 }
 
 TEST(URLEncoding, CopyrightCharacter) {
 	auto hexString = std::string("©");
 	auto result = URLEncoding::encode(hexString);
-	
+
 	EXPECT_EQ(result, "%c2%a9");
 }
 
 TEST(URLDecoding, CopyrightAddress) {
 	auto hexString = std::string("%c2%a9salzaverde%40github.com");
 	auto result = URLEncoding::decode(hexString);
-	
+
 	EXPECT_EQ(result, "©salzaverde@github.com");
 }
 
 TEST(URLEncoding, CopyrightAddress) {
 	auto hexString = std::string("©salzaverde@github.com");
 	auto result = URLEncoding::encode(hexString);
-	
+
 	EXPECT_EQ(result, "%c2%a9salzaverde%40github.com");
 }
-

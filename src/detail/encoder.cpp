768971637f1286cebd7bf883f343928cf8c6bee0
daniel <daniel.schuetz.lrt@gmail.com>
2023-01-02 15:48:23 +0100

    feat: Completed  encoder, all tests pass.

--- src/detail/encoder.cpp
+++ src/detail/encoder.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <regex>
 
+static std::string hexEntries = "0123456789abcdef";
+
 namespace salzaverde {
 	class Regex {
 	public:
@@ -41,19 +43,54 @@ namespace salzaverde {
 		}
 	};
 	
-	static std::string hexToString(const std::string &hex) {
-		int len = hex.length();
-		std::string output;
-		for(int i=0; i< len; i+=2) {
-			std::string byte = hex.substr(i,2);
-			char chr = (char) (int) strtol(byte.c_str(), nullptr, 16);
-			output.push_back(chr);
+	class Conversions {
+	public:
+		static std::string hexToString(const std::string &hex) {
+			int len = hex.length();
+			std::string output;
+			for(int i=0; i< len; i+=2) {
+				std::string byte = hex.substr(i,2);
+				char character = (char) (int) strtol(byte.c_str(), nullptr, 16);
+				output.push_back(character);
+			}
+			return output;
 		}
-		return output;
+
+		static std::string intToHex(int integer) {
+			auto hexIndex0 = integer / 16;
+			auto hexIndex1 = integer % 16;
+			
+			std::string result = "";
+			result += hexEntries[hexIndex0];
+			result += hexEntries[hexIndex1];
+
+			return result;
+		}
+
+		static std::string charToHex(const char& raw) {
+			auto integer = static_cast<uint8_t>(raw);
+			return intToHex(integer);
+		}
+	};
+
+	static bool isUnreserved(const char* character) {
+		auto regex = Regex(std::regex(R"([a-zA-Z0-9]|[-._~])"));
+		auto matches = regex.search(std::string(character));
+		if(matches.size() > 0 && matches[0].content[0] == *character) 
+			return true;
+
+		return false;
 	}
-	
+
 	std::string URLEncoding::encode(const std::string &raw) {
-		return "";
+		std::string result = "";
+		for(auto character : raw) {
+			if(isUnreserved(&character))
+				result += character;
+			else
+				result += "%" + Conversions::charToHex(character);
+		}
+		return result;
 	}
 	
 	std::string URLEncoding::decode(const std::string &encoded) {
@@ -63,9 +100,8 @@ namespace salzaverde {
 		std::string result = "";
 		
 		for(auto &match : matches) {
-			assert(match.groups.size() == 4);
 			if(! match.groups[1].empty()) result += match.groups[1];
-			if(! match.groups[2].empty()) result += hexToString(match.groups[2].substr(1));
+			if(! match.groups[2].empty()) result += Conversions::hexToString(match.groups[2].substr(1));
 			if(! match.groups[3].empty()) result += match.groups[3];
 		}
 		
